e0d63da95d5f29f81beaf7b3fbaa1942e064d835
Rafael Freitas <gothsonn@gmail.com>
2020-12-13 01:03:42 -0300

    base commit

---
+++ src/main/kotlin/com/example/springdemo/DockerController.kt
@@ -0,0 +1,18 @@
+package com.example.springdemo
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+    @RestController
+    @RequestMapping("/api")
+class DockerController {
+        @RequestMapping("/")
+        fun home(): String {
+            return "Welcome to Docker apis"
+        }
+
+        @RequestMapping("/hello")
+        fun helloWorld(): String {
+            return "Welcome to Docker apis, This is hello world in docker"
+        }
+}
---
+++ src/main/kotlin/com/example/springdemo/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package com.example.springdemo
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(SpringdemoApplication::class.java)
+    }
+
+}
